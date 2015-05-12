711b4dde53714b975d45dfee8ccaeb74b66a5ef9
Google <appengine@google.com>
2015-05-12 12:21:36 +0000

    12-May-2015 Google AppEngine Python SDK v1.9.20

--- google_appengine/wrapper_util.py
+++ google_appengine/wrapper_util.py
@@ -220,6 +220,7 @@ class Paths(object):
         os.path.join(dir_path, 'lib', 'requests'),
         os.path.join(dir_path, 'lib', 'six'),
         os.path.join(dir_path, 'lib', 'websocket'),
+        os.path.join(dir_path, 'lib', 'portpicker'),
         os.path.join(dir_path, 'lib', 'jinja2-2.6'),
         os.path.join(dir_path, 'lib', 'webob-1.2.3'),
         os.path.join(dir_path, 'lib', 'webapp2-2.5.1'),
