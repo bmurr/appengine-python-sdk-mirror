0c8e8e8870edb67cc14561291f351f767fd9a193
bmurr <bmurr@users.noreply.github.com>
2023-05-05 16:11:54 +0200

    Sort version list by build number.
    Also patch tar extractall for CVE-2007-4559

--- mirror_repo.py
+++ mirror_repo.py
@@ -114,7 +114,7 @@ class GoogleCloudSDKDownloader:
     @staticmethod
     def get_all_versions(*args):
         info = GoogleCloudSDKDownloader.get_version_json()
-        versions = {}
+        versions = []
         for cloud_sdk, version_components in info.items():
             component = version_components.get("app-engine-python-sdk")
 
@@ -132,9 +132,10 @@ class GoogleCloudSDKDownloader:
                 build_number,
                 f"app-engine-python-sdk_{version_string}.tar.gz",
             )
-            versions[version_string] = component_info
-
-        return versions
+            versions.append((version_string, component_info))
+            
+        versions = sorted(versions, key=lambda p: p[1].build_number, reverse=True)
+        return dict(versions)
 
     @staticmethod
     def list_versions(versions, args):
@@ -229,8 +230,26 @@ def extract_tar_files(tar_filename, output_directory=None):
                 yield member
 
     with tarfile.open(tar_filename, "r:gz") as _tar:
+        def is_within_directory(directory, target):
 
-        _tar.extractall(path=output_directory, members=members(_tar))
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+
+            return prefix == abs_directory
+
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+
+
+        safe_extract(_tar, path=output_directory, members=members(_tar))
 
 
 def commit_files(
